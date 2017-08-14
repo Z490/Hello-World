be4436be69bf134869a60df23e6232c5de59df12
ZX <490661700@qq.com>
2017-08-15 00:32:20 +0800

    sixth commit

--- hello.c
+++ hello.c
@@ -1,8 +1,4 @@
 #include<stdio.h>
 void main(){
-    printf("helloworld1");
-    printf("helloworld2");
-    printf("helloworld3");
-    printf("helloworld4");
-    printf("helloworld5");
+    int a=0;
 }
